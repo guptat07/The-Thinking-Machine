377f8ef10fb97d219a5604b52f9ae267fc59e219
Tyler Bullock <tylerbullock@ufl.edu>
2026-03-07 19:13:20 -0500

    added addons to gitignore, test.cs to see dialogic example

--- Scripts/test.cs
+++ Scripts/test.cs
@@ -3,4 +3,15 @@ using System;
 
 public partial class test : Node
 {
+    [Export] public NodePath DialogicSingletonPath = "/root/Dialogic";
+	[Export] public string DialoguePath = "res://Assets/Dialogue/test_timeline.dtl";
+
+    public override void _Ready()
+    {
+        var dlg = GetNodeOrNull<Node>(DialogicSingletonPath);
+        if(dlg == null){
+            GD.Print("SKDFJLKS");
+        }
+        dlg.Call("start", DialoguePath);
+    }
 }
